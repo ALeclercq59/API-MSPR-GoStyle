076315a60ca3c97d06028f74459d97a8c1185ad4
Anthony <a.leclercq55@live.fr>
2021-03-10 14:37:30 +0100

    Changement

--- src/main/java/com/gostyle/controllers/CouponController.java
+++ src/main/java/com/gostyle/controllers/CouponController.java
@@ -4,6 +4,8 @@ import com.gostyle.entities.Coupon;
 import com.gostyle.services.CouponService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,8 +31,14 @@ public class CouponController {
     }
 
     @GetMapping("/{id}")
-    public Coupon getOneCoupon(@PathVariable("id") Long id) {
-        return service.read(id);
+    public ResponseEntity<Coupon> getOneCoupon(@PathVariable("id") Long id) {
+        Coupon coupon = service.read(id);
+        if (coupon != null) {
+            return new ResponseEntity<>(coupon, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(coupon, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/info={info}")
